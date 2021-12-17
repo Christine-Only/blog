158240bfe73f98e06290ce1f14396474d68193fb
Christinesy <tsy17321289030@163.com>
2021-12-17 17:03:31 +0800

    fix: update remote url

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,4 @@ git init
 git add -A
 git commit -m "deploy脚本更新"
 
-git push --force git@github.com:Christine-Only/blog.git master:gh-pages
+git push --force https://github.com/Christine-Only/blog.git master:gh-pages
